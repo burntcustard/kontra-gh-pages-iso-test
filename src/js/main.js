663c8aafa952696f13405b36f595789882660aa9
burntcustard <burntcustard@gmail.com>
2022-03-12 23:51:45 +0000

    Deploying to gh-pages from @ burntcustard/kontra-gh-pages-iso-test@5aab0763798d531856ecbb38b3df9ad66a58340b 🚀

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import { init, Sprite, GameLoop } from 'kontra';
+
+let { canvas } = init();
+
+let sprite = Sprite({
+  x: 100,
+  y: 100,
+  dx: 2,
+  width: 20,
+  height: 40,
+  color: 'red'
+});
+
+let loop = GameLoop({
+  update() {
+    sprite.update();
+  },
+  render() {
+    sprite.render();
+  }
+});
+
+loop.start();
