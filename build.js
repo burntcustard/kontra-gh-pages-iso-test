616fce435ec3f1a77dfca62a10d48b3b879e5d5a
burntcustard <burntcustard@gmail.com>
2022-03-12 19:33:37 +0000

    Simplish build process for JS, with a few dependencies

---
+++ build.js
@@ -0,0 +1,32 @@
+import browserSync from "browser-sync";
+import esbuild from "esbuild";
+import kontra from "kontra";
+
+const bs = browserSync.create();
+
+const DEVMODE = process.argv.slice(2).includes('--watch');
+
+async function build() {
+  const result = await esbuild
+    .build({
+      bundle: true,
+      entryPoints: ['src/js/main.js'],
+      outdir: 'dist',
+      incremental: DEVMODE,
+      minify: !DEVMODE
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+  console.log("Build finished");
+}
+
+build();
+
+if (DEVMODE) {
+  bs.init({
+    server: true,
+    files: 'src/**',
+  });
+}
