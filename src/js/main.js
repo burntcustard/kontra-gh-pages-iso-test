49ff813c35fd41cf865e33dedfc00508285e2394
burntcustard <burntcustard@gmail.com>
2022-03-13 16:10:23 +0000

    Resizing canvas element

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 import { init, Sprite, GameLoop } from 'kontra';
 
-let { canvas } = init();
+let { canvas, context } = init();
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
 
 let sprite = Sprite({
   x: 100,
